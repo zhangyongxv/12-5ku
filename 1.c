ce8ad710800a13310b7886e3d4c0db45df9bd63f
zhangyongxv <2632739175@qq.com>
2023-12-07 10:59:14 +0800

    最新

---
+++ 1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void main(){
+    printf("测试文件");
+    printf("master push");
+    return;
+}
--- 3.c
+++ 3.c
@@ -1,6 +1,12 @@
+#include <stdio.h>
 void fun3(){
     
     printf("fun3");
     printf("在分支dev上加fun3");
     printf("github打印");
+}
+void main(){
+
+    fun3();
+    return;
 }
